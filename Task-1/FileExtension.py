bd043599927dbeec7bfd73fd7275a30f350aad05
Indranil1211 <75845410+Indranil1211@users.noreply.github.com>
2020-12-11 19:46:04 +0530

    Uploaded the file from my pc containing two codes
    one code is to estimate the area of a circle and other one is for identifying the extension of a file.

---
+++ Task-1/FileExtension.py
@@ -0,0 +1,9 @@
+a=str(input("Input the filename: "))
+if(a[-5:]==".java"):
+    print("The extension of the file is: 'Java' ")
+if(a[-2:]==".c"):
+    print("The extension of the file is: 'C' ")
+if(a[-3:]==".cc"):
+    print("The extension of the file is: 'C++' ")
+if(a[-3:]==".py"):
+    print("The extension of the file is: 'Python' ")
